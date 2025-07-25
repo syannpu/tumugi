0946278f724b81fbd4d533a507ccca554d80fe52
Your Name <you@example.com>
2025-07-25 22:02:18 +0900

    add:投稿機能

--- Gemfile
+++ Gemfile
@@ -58,3 +58,4 @@ group :test do
   gem "selenium-webdriver"
 end
   gem "devise"
+  gem 'rails-i18n', '~> 7.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_23_001116) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_25_072352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +35,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_23_001116) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
