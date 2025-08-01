a6e67d47612c6a82937b856df94b5d5193401595
Your Name <you@example.com>
2025-08-01 10:39:54 +0900

    add:atmosphereカテゴリー

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index new create show edit update destroy]
   resources :marches, only: %i[index new create show edit update destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
